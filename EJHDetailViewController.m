c66c113b8c7bd689a00588fe7d0ecad6ed24644c
Emily Hoehne <emilyhoehne@theglutconomist.home>
2014-09-17 20:33:25 -0600

    Day five

--- EJHDetailViewController.m
+++ EJHDetailViewController.m
@@ -12,6 +12,7 @@
 @property (strong, nonatomic) IBOutlet UITextField *textField;
 @property (strong, nonatomic) IBOutlet UITextView *textView;
 @property (strong, nonatomic) IBOutlet UIButton *button; 
+@property (strong, nonatomic) IBOutlet UIToolbar *barButton;
 
 @end
 
